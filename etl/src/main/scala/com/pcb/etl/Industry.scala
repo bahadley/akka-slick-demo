0969fb0ce2c41fc66ff6ab31c30820171ea8331e
Ben Hadley <bhadley2@gmail.com>
2016-10-05 15:23:49 -0700

    Add basic processing

--- etl/src/main/scala/com/pcb/etl/Industry.scala
+++ etl/src/main/scala/com/pcb/etl/Industry.scala
@@ -4,6 +4,7 @@ import akka.actor.{Actor, ActorLogging}
 import akka.camel.{CamelMessage, Consumer}
 import akka.pattern.pipe
 import com.pcb.messages.CreateIndustry
+import scala.io.Source
 
 class Industry extends Actor with ActorLogging with Consumer {
 
@@ -11,7 +12,12 @@ class Industry extends Actor with ActorLogging with Consumer {
 
   def receive = {
     case msg: CamelMessage => {
-      println("received %s" format msg.bodyAs[String])
+      //println("received %s" format msg.bodyAs[String])
+      
+      val lines = msg.bodyAs[String].split("\\r?\\n")
+      lines foreach println
     }
   }
+
+  def parseRow(arr:Array[String]) = CreateIndustry(arr(0), arr(1), arr(2))
 }
