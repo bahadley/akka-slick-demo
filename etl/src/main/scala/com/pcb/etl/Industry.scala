92ee53e324e3150bd80d166a41affa41b5f7eeaa
Ben Hadley <bhadley2@gmail.com>
2016-10-05 17:43:58 -0700

    Select only named file

--- etl/src/main/scala/com/pcb/etl/Industry.scala
+++ etl/src/main/scala/com/pcb/etl/Industry.scala
@@ -4,20 +4,23 @@ import akka.actor.{Actor, ActorLogging}
 import akka.camel.{CamelMessage, Consumer}
 import akka.pattern.pipe
 import com.pcb.messages.CreateIndustry
-import scala.io.Source
 
 class Industry extends Actor with ActorLogging with Consumer {
 
-  def endpointUri = "file:data/input?delete=true"
+  val fileName = "Industry.txt"
+
+  def endpointUri = s"file:data/input?include=${fileName}&delete=true"
 
   def receive = {
     case msg: CamelMessage => {
-      //println("received %s" format msg.bodyAs[String])
-      
       val lines = msg.bodyAs[String].split("\\r?\\n")
-      lines foreach println
+      for (line <- lines) {
+        val v = line.split('|')
+        println(genMsg(v))
+      }
     }
   }
 
-  def parseRow(arr:Array[String]) = CreateIndustry(arr(0), arr(1), arr(2))
+  def genMsg(arr:Array[String]) = 
+    CreateIndustry(arr(0), arr(1), arr(2))
 }
