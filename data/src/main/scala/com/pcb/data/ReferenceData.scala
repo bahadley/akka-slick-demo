6f8490d15d0b847050eb021cb116393a3a0a1574
Ben Hadley <bhadley2@gmail.com>
2016-10-07 14:57:10 -0700

    Add remainder of reference data sql and message handlers

--- data/src/main/scala/com/pcb/data/ReferenceData.scala
+++ data/src/main/scala/com/pcb/data/ReferenceData.scala
@@ -10,44 +10,69 @@ class ReferenceData extends Actor with ActorLogging {
   import context.dispatcher
 
   var db = None : Option[Database]
+  val ERR_MSG_DB = "Connection pool is uninitialized" 
 
   override def preStart(): Unit = {
     db = Some(Database.forConfig("tpcdi"))
     super.preStart()
   }
 
-  def receive = {
-    case msg: CreateIndustry =>
-      db match {
-        case None => throw new NullPointerException("Database is uninitialized") 
-        case Some(db) => pipe(db.run(insert(msg))) to sender()
-      }
-    case msg: DeleteIndustry =>
-      db match {
-        case None => throw new NullPointerException("Database is uninitialized")
-        case Some(db) => pipe(db.run(delete(msg))) to sender()
-      }
-    case msg: CountIndustry =>
-      db match {
-        case None => throw new NullPointerException("Database is uninitialized")
-        case Some(db) => pipe(db.run(count(msg))) to sender()
-      }
-  }
-
   override def postStop(): Unit = {
     db match {
-      case None => throw new NullPointerException("Database is uninitialized")
+      case None => throw new NullPointerException(ERR_MSG_DB)
       case Some(db) => db.close 
     }
     super.postStop()
   }
 
-  def insert(i: CreateIndustry): DBIO[Int] =
-    sqlu"insert into industry values (${i.in_id}, ${i.in_name}, ${i.in_sc_id})"
+  def receive = {
+    case msg: CreateIndustry =>
+      db match {
+        case None => throw new NullPointerException(ERR_MSG_DB) 
+        case Some(db) => pipe(db.run(insertIn(msg))) to sender()
+      }
+    case msg: CreateStatusType =>
+      db match {
+        case None => throw new NullPointerException(ERR_MSG_DB) 
+        case Some(db) => pipe(db.run(insertSt(msg))) to sender()
+      }
+    case msg: CreateTaxRate =>
+      db match {
+        case None => throw new NullPointerException(ERR_MSG_DB) 
+        case Some(db) => pipe(db.run(insertTx(msg))) to sender()
+      }
+    case msg: CreateTradeType =>
+      db match {
+        case None => throw new NullPointerException(ERR_MSG_DB) 
+        case Some(db) => pipe(db.run(insertTt(msg))) to sender()
+      }
+    case msg: DeleteIndustry =>
+      db match {
+        case None => throw new NullPointerException(ERR_MSG_DB)
+        case Some(db) => pipe(db.run(deleteIn(msg))) to sender()
+      }
+    case msg: CountIndustry =>
+      db match {
+        case None => throw new NullPointerException(ERR_MSG_DB)
+        case Some(db) => pipe(db.run(countIn(msg))) to sender()
+      }
+  }
 
-  def delete(i: DeleteIndustry): DBIO[Int] =
-    sqlu"delete from industry where in_id = ${i.in_id}"
+  def insertIn(in: CreateIndustry): DBIO[Int] =
+    sqlu"insert into industry values (${in.in_id}, ${in.in_name}, ${in.in_sc_id})"
 
-  def count(i: CountIndustry): DBIO[Int] =
-    sql"select count(*) from industry where in_id = ${i.in_id}".as[Int].head
+  def deleteIn(in: DeleteIndustry): DBIO[Int] =
+    sqlu"delete from industry where in_id = ${in.in_id}"
+
+  def countIn(in: CountIndustry): DBIO[Int] =
+    sql"select count(*) from industry where in_id = ${in.in_id}".as[Int].head
+
+  def insertSt(st: CreateStatusType): DBIO[Int] =
+    sqlu"insert into statustype values (${st.st_id}, ${st.st_name})"
+
+  def insertTx(tx: CreateTaxRate): DBIO[Int] =
+    sqlu"insert into taxrate values (${tx.tx_id}, ${tx.tx_name}, ${tx.tx_rate})"
+
+  def insertTt(tt: CreateTradeType): DBIO[Int] =
+    sqlu"insert into tradetype values (${tt.tt_id}, ${tt.tt_name}, ${tt.tt_is_sell}, ${tt.tt_is_market})"
 }
